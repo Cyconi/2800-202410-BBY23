97ad94c5b1e57794bed971067a772a83b3971528
Cyconi <cyconiexo@gmail.com>
2024-05-14 14:12:34 -0700

    fixed typo

--- modules/passport.js
+++ modules/passport.js
@@ -1,6 +1,6 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require("bcrypt");
-const User = require('../modules/user');
+const User = require('./user');
 
 module.exports = function (passport) {
     passport.use(new LocalStrategy(
