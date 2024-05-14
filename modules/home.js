12c3515330d6d8e4b90ad3720fb7d402ddcb51b1
Cyconi <cyconiexo@gmail.com>
2024-05-14 14:26:44 -0700

    re added mongo code - cope

--- modules/home.js
+++ modules/home.js
@@ -5,11 +5,41 @@ const passport = require("passport");
 const User = require('./user');
 
 router.post('/login', (req, res, next) => {
-    // ...
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return res.status(500).send('Internal Server Error');
+        }
+        if (!user) {
+            return res.status(401).send(info.message);
+        }
+        req.login(user, loginErr => {
+            if (loginErr) {
+                return res.status(500).send('Error logging in');
+            }
+            res.redirect('/index.html');
+        });
+    })(req, res, next);
 });
 
 router.post('/signup', async (req, res) => {
-    // ...
+    const { username, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).send('Signup Failed: User already exists with that username.');
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, password: hashedPassword });
+        await newUser.save();
+        req.login(newUser, loginErr => {
+            if (loginErr) {
+                return res.status(500).send('Error during signup process.');
+            }
+            res.redirect('/index.html');
+        });
+    } catch (err) {
+        res.status(500).send('Error during signup process.');
+    }
 });
 
 module.exports = router;
--- modules/passport.js
+++ modules/passport.js
@@ -5,15 +5,32 @@ const User = require('./user');
 module.exports = function (passport) {
     passport.use(new LocalStrategy(
         async (username, password, done) => {
-            // ...
+            try {
+                const user = await User.findOne({ username });
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+                return done(null, user);
+            } catch (err) {
+                return done(err);
+            }
         }
     ));
 
     passport.serializeUser((user, done) => {
-        // ...
+        done(null, user.id);
     });
 
     passport.deserializeUser(async (id, done) => {
-        // ...
+        try {
+            const user = await User.findById(id);
+            done(null, user);
+        } catch (err) {
+            done(err);
+        }
     });
 };
