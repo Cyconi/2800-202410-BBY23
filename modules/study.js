2b89ae40ba78c59519cae3f83136c31753d01750
Athen <cyconiexo@gmail.com>
2024-05-15 13:24:11 -0700

    fixed study hamburbur icon

--- modules/study.js
+++ modules/study.js
@@ -4,6 +4,13 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 const User = require('./user');
 
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+router.use(express.static('public'));
+router.use("/js", express.static("./webapp/public/js"));
+router.use("/css", express.static("./webapp/public/css"));
+router.use("/img", express.static("./webapp/public/img"));
+
 router.get('/', (req, res) => {
     res.render('study-page');
 });
