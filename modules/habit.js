6ef9dd48bb90f6a0ba77ebdbb74593b59b500329
Radmir Garipov <radmirgaripovrss@gmail.com>
2024-05-15 14:48:38 -0700

    Made it so that you can manually change url to maneuver around habit tracker

--- modules/habit.js
+++ modules/habit.js
@@ -4,6 +4,14 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 const User = require('./user');
 const Habit = require('./habitSchema');
+
+function authenticated(req, res, next){
+    if(req.isAuthenticated()){
+        next();
+    }
+    res.redirect('/');
+}
+
 router.get('/', (req, res) => {
     if(!req.isAuthenticated()){
         res.redirect('/');
@@ -12,6 +20,24 @@ router.get('/', (req, res) => {
     res.render('habitIndex');
 });
 router.post('/goodHabit', async (req, res) => {
+    try {
+        const goodHabits = await Habit.find({ email: req.user.email, good: true });
+        res.render('habitList', { habits: goodHabits, good: true });
+    } catch (err) {
+        res.status(500).send("Error retrieving good habits");
+    }
+});
+
+router.get('/badHabit', authenticated, async (req, res) => {
+    try {
+        const goodHabits = await Habit.find({ email: req.user.email, good: false });
+        res.render('habitList', { habits: goodHabits, good: false});
+    } catch (err) {
+        res.status(500).send("Error retrieving bad habits");
+    }
+});
+
+router.get('/goodHabit', authenticated, async (req, res) => {
     try {
         const goodHabits = await Habit.find({ email: req.user.email, good: true });
         res.render('habitList', { habits: goodHabits, good: true });
@@ -31,6 +57,13 @@ router.post('/badHabit', async (req, res) => {
 router.post('/badHabitAdd', (req, res) => {
     res.render('addHabit', { good: false });
 })
+
+router.get('/badHabitAdd', authenticated, (req, res) => {
+    res.render('addHabit', { good: false });
+});
+router.get('/goodHabitAdd', authenticated, (req, res) => {
+    res.render('addHabit', { good: true });
+});
 router.post('/goodHabitAdd', (req, res) => {
     res.render('addHabit', { good: true });
 })
