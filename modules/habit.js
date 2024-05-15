604bd7fb0257f1f989d4f5feeb1b4797567cb5ec
Radmir Garipov <radmirgaripovrss@gmail.com>
2024-05-14 18:40:37 -0700

    made functioning buttons and added a new file

--- modules/habit.js
+++ modules/habit.js
@@ -7,6 +7,18 @@ const User = require('./user');
 router.get('/', (req, res) => {
     res.render('habitIndex');
 });
-
+router.post('/goodHabit', (req, res) => {
+    res.render('habitLists', { good: true });
+})
+router.post('/badHabit', (req, res) => {
+    res.render('habitLists', { good: false});
+})
+router.post('/badHabitAdd', (req, res) => {
+    res.render('addsAHabit', {good: false});
+})
+router.post('/goodHabitAdd', (req, res) => {
+    res.render('addsAHabit', {good: true});
+})
+router.post()
 module.exports = router;
 
