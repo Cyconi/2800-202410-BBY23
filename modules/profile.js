dc8291a169923ebf4ab6654ef7c6745ffb39dfd2
Radmir Garipov <radmirgaripovrss@gmail.com>
2024-05-15 21:28:46 -0700

    Made it so that the profile page displays all user data

---
+++ modules/profile.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const User = require('./user');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
+
+router.get('/', (req, res) => {
+    res.render('profile');
+});
+
+router.post('/profileElements', ensureAuthenticated, (req, res) => {
+    const username = req.user.username;
+    const name = req.user.name;
+    const email = req.user.email;
+    res.json({username: username, name: name, email: email});
+});
