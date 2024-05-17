0a0fe3abd1841be46c4770c4eea8b6a64ee9ad7b
Qu1vv <jeremy_testa@hotmail.com>
2024-05-16 20:27:46 -0700

    nothing important

--- modules/home.js
+++ modules/home.js
@@ -49,6 +49,7 @@ router.post('/forgot', async (req, res) => {
 });
 
 router.get('/reset/:token', async (req, res) => {
+    
     const user = await User.findOne({
         resetPassword: req.params.token,
         resetPasswordDate: { $gt: Date.now() }
